e538f5b1d4b53899f7e681af2a3c32a9e27fd976
Ali Eldeeb <aeldeeb@fordham.edu>
2023-01-21 13:10:58 -0800

    Setting up project files

---
+++ AnimeClassifier/AnimeClassifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeClassifier
+//
+//  Created by Ali Eldeeb on 1/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
